94feff42419e2ca5b555ee01f46033c6f884c3fe
chrirathana <chrisrathana63@gmail.com>
2025-09-23 02:41:10 +0700

    Create caches.js
    Signed-off-by: chrirathana <chrisrathana63@gmail.com>

---
+++ javascript/caches.js
@@ -0,0 +1,44 @@
+self.addEventListener("install", (event) => {
+  event.waitUntil(
+    caches
+      .open("v1")
+      .then((cache) =>
+        cache.addAll([
+          "/",
+          "/index.html",
+          "/style.css",
+          "/app.js",
+          "/image-list.js",
+          "/avatar-logo.jpg",
+          "/gallery/jpg",
+          "/gallery/jpg",
+          "/gallery/jpg",
+        ]),
+      ),
+  );
+});
+
+self.addEventListener("fetch", (event) => {
+  event.respondWith(
+    caches.match(event.request).then((response) => {
+      // caches.match() always resolves
+      // but in case of success response will have value
+      if (response !== undefined) {
+        return response;
+      }
+      return fetch(event.request)
+        .then((response) => {
+          // response may be used only once
+          // we need to save clone to put one copy in cache
+          // and serve second one
+          let responseClone = response.clone();
+
+          caches
+            .open("v1")
+            .then((cache) => cache.put(event.request, responseClone));
+          return response;
+        })
+        .catch(() => caches.match("/gallery/myLittleVader.jpg"));
+    }),
+  );
+});
