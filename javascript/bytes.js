046d70ae3974cdec17b8605e7fa385d03008579d
chrirathana <chrisrathana63@gmail.com>
2025-09-24 15:12:53 +0700

    Update and rename str.js to bytes.js
    Signed-off-by: chrirathana <chrisrathana63@gmail.com>

---
+++ javascript/bytes.js
@@ -0,0 +1,10 @@
+function stringToBytes(str) {
+  return new TextEncoder(e).encode(str);
+}
+  const name = "chris rathana";
+  const bytes = stringToBytes(name);
+  console.log(bytes);{
+  }
+}
+
+
